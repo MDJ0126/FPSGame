156a12700f013bb051a158823524b5429666dfb1
문동준 <ehdwns0126@gmail.com>
2024-02-11 03:37:38 +0900

    수정

--- Assets/Scripts/Character/Base/CharacterFootIK.cs
+++ Assets/Scripts/Character/Base/CharacterFootIK.cs
@@ -5,53 +5,48 @@ using UnityEngine.Animations.Rigging;
 [RequireComponent(typeof(TwoBoneIKConstraint))]
 public class CharacterFootIK : MonoBehaviour
 {
-    private const float MOTION_SPEED = 4f;
-    private const float STEP_HEIGHT = 0.5f;
-
     #region Inspector
 
-    public Transform forward;
-    public Transform back;
+    public Vector3 offset;
 
-	#endregion
+    #endregion
 
-	public TwoBoneIKConstraint TwoBoneIKConstraint { get; private set; } = null;
-    private Vector3 _oldPosition = Vector3.zero;
-    public Vector3 NewPosition { get; private set; } = Vector3.zero;
-    private Vector3 _currentPos = Vector3.zero;
-    private float _lerp = 1f;
+    public TwoBoneIKConstraint TwoBoneIKConstraint { get; private set; } = null;
 
     private void Awake()
     {
-        TwoBoneIKConstraint = GetComponent<TwoBoneIKConstraint>();
-        _oldPosition = NewPosition = _currentPos = TwoBoneIKConstraint.data.target.position;
+        this.TwoBoneIKConstraint = GetComponent<TwoBoneIKConstraint>();
     }
 
     private void FixedUpdate()
     {
-        if (_lerp < 1f)
+        this.TwoBoneIKConstraint.weight = 0f;
+        Vector3 start = this.TwoBoneIKConstraint.data.tip.position + offset;
+        Vector3 dir = Vector3.down;
+        const float DISTANCE = 1f;
+        Debug.DrawRay(start, dir * DISTANCE);
+        if (Physics.Raycast(start, dir, out RaycastHit hit, DISTANCE))
         {
-            _lerp += Time.deltaTime * MOTION_SPEED;
-            _lerp = Mathf.Min(_lerp, 1f);
-            _currentPos = Vector3.Lerp(_oldPosition, NewPosition, _lerp);
-            _currentPos.y = Mathf.Sin(Mathf.PI * _lerp) * STEP_HEIGHT;
+            this.TwoBoneIKConstraint.data.target.position = hit.point + offset;
+            float diff = Vector3.Distance(hit.point, start);
+            Debug.Log(diff);
+            if (diff <= 0.25f)
+            {
+                this.TwoBoneIKConstraint.weight = 1f;
+            }
         }
-        TwoBoneIKConstraint.data.target.position = _currentPos;
     }
 
     public void Move(Vector3 wolrdPosition)
     {
-        _lerp = 0f;
-        _oldPosition = NewPosition;
-        NewPosition = wolrdPosition;
     }
 
     private void OnDrawGizmos()
     {
-        if (!Application.isPlaying) return;
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(_oldPosition, 0.2f);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(NewPosition, 0.2f);
+        //if (!Application.isPlaying) return;
+        //Gizmos.color = Color.yellow;
+        //Gizmos.DrawWireSphere(_oldPosition, 0.2f);
+        //Gizmos.color = Color.blue;
+        //Gizmos.DrawWireSphere(NewPosition, 0.2f);
     }
 }
--- Assets/Scripts/Character/Base/CharacterHipController.cs
+++ Assets/Scripts/Character/Base/CharacterHipController.cs
@@ -10,67 +10,7 @@ public class CharacterHipController : MonoBehaviour
 
     #endregion
 
-    private bool _isMoveing = false;
-    private bool _isIdle => !_isMoveing;
-
-    private Vector3 _center = Vector3.zero;
-    private Vector3 _currentCenter = Vector3.zero;
-
-    private void Start()
-    {
-        UpdateCenter();
-    }
-
-    private void Update()
-    {
-        Vector3 start = leftFootIK.TwoBoneIKConstraint.data.target.position;
-        Vector3 dir = (rightFootIK.TwoBoneIKConstraint.data.target.position - leftFootIK.TwoBoneIKConstraint.data.target.position).normalized;
-        float distance = Vector3.Distance(leftFootIK.TwoBoneIKConstraint.data.target.position, rightFootIK.TwoBoneIKConstraint.data.target.position);
-        Debug.DrawRay(start, dir * distance, Color.green);
-    }
-
-    private void UpdateCenter()
-    {
-        Vector3 left = leftFootIK.NewPosition;
-        left.y = 0f;
-        Vector3 right = rightFootIK.NewPosition;
-        right.y = 0f;
-        _center = Vector3.Lerp(left, right, 0.5f);
-        _center.y = 0f;
-        _currentCenter = this.transform.position;
-        _currentCenter.y = _center.y;
-    }
-
     private void FixedUpdate()
     {
-        UpdateCenter();
-        if (Vector3.Distance(_center, _currentCenter) > 0.3f)
-        {
-            Vector3 dir = _currentCenter - _center;
-            float centerToLeftDistance = Vector3.Distance(_currentCenter, leftFootIK.NewPosition);
-            float centerToRightDistance = Vector3.Distance(_currentCenter, rightFootIK.NewPosition);
-
-            if (centerToLeftDistance < centerToRightDistance)
-            {
-                rightFootIK.Move(rightFootIK.forward.position);
-            }
-            else
-            {
-                leftFootIK.Move(leftFootIK.forward.position);
-            }
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (!Application.isPlaying) return;
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(leftFootIK.TwoBoneIKConstraint.data.target.position, 0.2f);
-        Gizmos.DrawWireSphere(rightFootIK.TwoBoneIKConstraint.data.target.position, 0.2f);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(_center, 0.05f);
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(_currentCenter, 0.05f);
     }
 }
